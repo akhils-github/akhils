70335f50abe0a803550f9560c84ab74514f347c6
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-07-22 16:38:17 -0300

    9: add chat button on that is absolute on the bottom right

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -1,11 +1,15 @@
+"use client";
+
 import Link from "next/link";
 import Image from "next/image";
+import { useState } from "react";
 import { Footer } from "@/components/Footer";
+import { ChatButton } from "@/components/ChatButton";
 
 export default function LayoutPage ({ children }: { children: React.ReactNode }) {
-
+    const [isOpen, setIsOpen] = useState(false);
     return (
-        <main className="min-h-screen flex flex-col items-center justify-center bg-zinc-900 px-4 py-8 text-zinc-50">
+        <main className="min-h-screen relative flex flex-col items-center justify-center bg-zinc-900 px-4 py-8 text-zinc-50">
           <Link href="/">
             <Image
               width={20}
@@ -14,10 +18,11 @@ export default function LayoutPage ({ children }: { children: React.ReactNode })
               src="/logo.svg"
               priority={true}
               className="h-14 w-14 text-center mb-4"
-              alt="Christopher Alonzo Initialials Logo"
+              alt="Christopher Alonzo Initials Logo"
             />
           </Link>
           {children}
+          <ChatButton />
           <Footer />
         </main>
     )
--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -1,5 +1,7 @@
 import { Homepage } from "@/components/homepage";
 
 export default function Home() {
-  return (<Homepage />);
+  return (
+    <Homepage 
+  />);
 }
