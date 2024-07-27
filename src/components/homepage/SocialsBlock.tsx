e445c6ad753342aafdf8d1660319f105a20a2a64
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-07-26 21:44:56 -0300

    fix the label on the social blocks

--- src/components/homepage/SocialsBlock.tsx
+++ src/components/homepage/SocialsBlock.tsx
@@ -4,6 +4,8 @@ import { SiGithub, SiLinkedin } from "react-icons/si";
 import { FiBriefcase } from "react-icons/fi";
 import { FaGraduationCap } from "react-icons/fa6";
 
+
+
 export const SocialsBlock = () => {
   return (
     <>
@@ -12,67 +14,75 @@ export const SocialsBlock = () => {
           rotate: "2.5deg",
           scale: 1.1,
         }}
-        className="group col-span-6  bg-blue-500 md:col-span-3"
+        className="group overflow-hidden col-span-6 bg-blue-500 md:col-span-3"
       >
         <Link
           href="https://www.linkedin.com/in/lonzochris"
           rel="noopener noreferrer"
           target="_blank"
-          className="grid h-full place-content-center text-2xl md:text-4xl text-white"
+          className="relative h-full flex flex-col gap-2 items-center justify-center text-white overflow-hidden transition-all"
         >
-          <SiLinkedin />
+          <SiLinkedin className="group-hover:translate-x-40 mx-auto text-2xl md:text-4xl font-black text-center transition duration-500 whitespace-nowrap"/>
+          <span className="-translate-x-60 text-sm md:text-xl font-semibold group-hover:translate-x-0 flex items-center justify-center absolute inset-2 transition-all duration-500 text-zinc-50 z-20" >
+              LinkedIn
+          </span>
         </Link>
-        <span 
-          className="-mt-10 hidden group-hover:block transition-all duration-400 ease-in-out text-center text-sm text-zinc-50 font-semibold leading-tight">
-            LinkedIn
-        </span>
       </Block>
       <Block
         whileHover={{
           rotate: "-2.5deg",
           scale: 1.1,
         }}
-        className="group  col-span-6 bg-neutral-800 md:col-span-3"
+        className="group overflow-hidden col-span-6 bg-neutral-800 md:col-span-3"
       >
         <Link
           href="https://www.github.com/achris-alonzo30"
           rel="noopener noreferrer"
           target="_blank"
-          className="grid h-full place-content-center text-2xl md:text-4xl text-white"
+          className="relative h-full flex flex-col gap-2 items-center justify-center text-white overflow-hidden transition-all"
         >
-          <SiGithub />
+          <SiGithub className="group-hover:translate-x-40 mx-auto text-2xl md:text-4xl font-black text-center transition duration-500 whitespace-nowrap"/>
+          <span className="-translate-x-60 text-sm md:text-xl font-semibold group-hover:translate-x-0 flex items-center justify-center absolute inset-2 transition-all duration-500 text-zinc-50 z-20" >
+              Github
+          </span>
         </Link>
-        <span className="-mt-10 hidden  group-hover:block transition-all duration-400 ease-in-out  text-center text-sm text-zinc-50 font-semibold leading-tight">Github</span>
+        
       </Block>
       <Block
         whileHover={{
           rotate: "-2.5deg",
           scale: 1.1,
         }}
-        className="group col-span-6 bg-emerald-500 md:col-span-3"
+        className="group overflow-hidden col-span-6 bg-emerald-500 md:col-span-3"
       >
         <Link
           href="/projects"
-          className="grid h-full place-content-center text-2xl md:text-4xl text-white"
+          className="mt-0 h-full flex flex-col gap-2 items-center justify-center text-white overflow-hidden transition-all"
         >
-          <FiBriefcase />
+          <FiBriefcase className="group-hover:translate-x-40 mx-auto text-2xl md:text-4xl font-black text-center transition duration-500 whitespace-nowrap"/>
+          <span className="-translate-x-60 text-sm md:text-xl font-semibold group-hover:translate-x-0 flex items-center justify-center absolute inset-2 transition-all duration-500 text-zinc-50 z-20" >
+              Projects
+          </span>
         </Link>
-        <span className="-mt-11 hidden text-center group-hover:block transition-all duration-400 ease-in-out  text-sm text-zinc-50 font-semibold leading-tight">Projects</span>
+        
       </Block>
       <Block
         whileHover={{
           rotate: "2.5deg",
           scale: 1.1,
         }}
-        className="group col-span-6 bg-rose-500 md:col-span-3"
+        className="group overflow-hidden col-span-6 bg-rose-500 md:col-span-3"
       >
         <Link
           href="/awards"
-          className="grid h-full place-content-center text-2xl md:text-4xl text-white"
+          className="h-full flex flex-col gap-2 items-center justify-center text-white overflow-hidden transition-all"
         >
-          <FaGraduationCap  />
+          <FaGraduationCap className="group-hover:translate-x-40 mx-auto text-2xl md:text-4xl font-black text-center transition duration-500 whitespace-nowrap" />
+          <span className="-translate-x-60 text-sm md:text-xl font-semibold group-hover:translate-x-0 flex items-center justify-center absolute inset-2 transition-all duration-500 text-zinc-50 z-20" >
+              Education
+          </span>
         </Link>
-        <span className="-mt-12 text-center hidden group-hover:block transition-all duration-400 ease-in-out  text-sm text-zinc-50 font-semibold leading-tight">Education</span>
+        
       </Block>
     </>
   );
