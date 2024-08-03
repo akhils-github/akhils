f2e40f991d4a0ec62f8ab1eb41c9c5f13925470b
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-08-03 15:12:33 -0300

    fix the font size on the About Block section

--- src/components/homepage/AboutBlock.tsx
+++ src/components/homepage/AboutBlock.tsx
@@ -3,9 +3,9 @@ import { Block } from "./Block";
 export const AboutBlock = () => {
   return (
     <Block className="col-span-12 text-lg md:text-xl lg:text-3xl leading-snug">
-      <p>
+      <p className="text-[12px] sm:text-[14px] md:text-base">
         I build things that solves problems. {" "}
-        <span className="text-base text-zinc-400">
+        <span className="text-zinc-400">
           I&apos;m a full stack developer and entrepreneur who&apos;s passionate about creating solutions. 
           Proficient in both front-end and back-end that brings creative ideas to code. 
         </span>
