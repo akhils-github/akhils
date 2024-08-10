ee03126446106d111aa2069993d5d48565f106f2
achris-alonzo30 <achris.alonzo30@gmail.com>
2024-08-10 11:20:19 -0300

    improve the grammar on the AboutSection block

--- src/components/homepage/AboutBlock.tsx
+++ src/components/homepage/AboutBlock.tsx
@@ -3,11 +3,11 @@ import { Block } from "./Block";
 export const AboutBlock = () => {
   return (
     <Block className="col-span-12 text-lg md:text-xl lg:text-3xl leading-snug">
-      <p className="text-[14px] md:text-base">
-        I build things that solves problems. {" "}
+      <p>
+        I build things that solve problems. {" "}
         <span className="text-zinc-400">
-          I&apos;m a full stack developer and entrepreneur who&apos;s passionate about creating solutions. 
-          Proficient in both front-end and back-end that brings creative ideas to code. 
+          I&apos;m a full-stack developer and entrepreneur who&apos;s passionate about creating solutions. 
+          Proficient in both front-end and back-end, bringing creative ideas to code.
         </span>
       </p>
     </Block>
