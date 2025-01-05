2c263b8236be6fcb89359164601c92b5bed21045
Akhil S <106079337+akhils-github@users.noreply.github.com>
2025-01-05 22:27:36 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 
      <body className="bg-gray-100 dark:bg-gray-800">
   <div className="min-h-screen flex flex-col justify-center items-center">
-    <img src="https://www.svgrepo.com/show/426192/cogs-settings.svg" alt="Logo" className="mb-8 h-40">
+    <img src="https://www.svgrepo.com/show/426192/cogs-settings.svg" alt="Logo" className="mb-8 h-40"/>
     <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold text-center text-gray-700 dark:text-white mb-4">Site is under maintenance</h1>
     <p className="text-center text-gray-500 dark:text-gray-300 text-lg md:text-xl lg:text-2xl mb-8">We're working hard to improve the user experience. Stay tuned!</p>
     <div className="flex space-x-4">
