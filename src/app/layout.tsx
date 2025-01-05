3df4665ee1c3f0b1dcbe2970fc464446f484f713
Akhil S <106079337+akhils-github@users.noreply.github.com>
2025-01-05 22:23:09 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,21 +17,18 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${font.className} antialiased`}>
 
-     
-
-<div class="container">
-   <div class="box">
-    <div class="animation">
-     <div class="one spin-one"></div>
-     <div class="two spin-two"></div>
-     <div class="three spin-one"></div>
+     <body class="bg-gray-100 dark:bg-gray-800">
+  <div class="min-h-screen flex flex-col justify-center items-center">
+    <img src="https://www.svgrepo.com/show/426192/cogs-settings.svg" alt="Logo" class="mb-8 h-40">
+    <h1 class="text-4xl md:text-5xl lg:text-6xl font-bold text-center text-gray-700 dark:text-white mb-4">Site is under maintenance</h1>
+    <p class="text-center text-gray-500 dark:text-gray-300 text-lg md:text-xl lg:text-2xl mb-8">We're working hard to improve the user experience. Stay tuned!</p>
+    <div class="flex space-x-4">
+      <a href="#" class="bg-gray-800 hover:bg-gray-700 text-white font-bold py-3 px-6 rounded dark:bg-gray-700 dark:hover:bg-gray-600">Contact Us</a>
+      <a href="#" class="border-2 border-gray-800 text-black font-bold py-3 px-6 rounded dark:text-white dark:border-white">Reload</a>
     </div>
-  <h1>Under maintenance</h1>
-  <p>Update in progress.  Please run installer to finish update.</p>
-  <p>Maintenance screen for<a href="https://www.ladesk.com/?utm_source=maintenance&utm_campaign=maintenance/" target="_blank">LiveAgent</a></p>
   </div>
-</div>
-        {children}
+</body>
+{/*         {children} */}
       </body>
       <GoogleAnalytics gaId="G-YS5KNVNWL4" />
     </html>
